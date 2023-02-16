1ba9b22dc2012fe5a84b349796ad8c581c25d32a
andahmed371 <andahmed371@gmail.com>
2023-02-16 14:47:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main -A program that prints aline with put function
- * Return 0:(Success)
+ * Return: 0(Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
